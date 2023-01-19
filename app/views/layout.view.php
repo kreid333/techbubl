65a7ea10d85d224fccd0f71f59a4814e8c460001
kreid333 <kairetech@gmail.com>
2023-01-19 17:33:12 -0500

    edited logo, navbar, and mobile hamburger menu; created main posts section and aside section; added footer

--- app/views/layout.view.php
+++ app/views/layout.view.php
@@ -8,12 +8,12 @@
     <link rel="stylesheet" href="public/assets/css/reset.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=Questrial&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;700&family=Questrial&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="public/assets/css/layout.css">
+    <link rel="stylesheet" href="public/assets/css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <script src="public/assets/js/scripts.js" defer></script>
-    <title>Document</title>
+    <title>TechBubl</title>
 </head>
 
 <!-- NAVBAR -->
@@ -43,7 +43,7 @@
     </div>
     <a href="/search" class="search">
         <i class="fa-solid fa-magnifying-glass"></i>
-</a>
+    </a>
     <button class="hamburger">
         <div class="bar"></div>
     </button>
@@ -64,7 +64,47 @@
 </div>
 
 <body>
-    <?php require("$area/$name.view.php") ?>
+    <div class="wrapper">
+        <div class="main-section">
+            <?php require("$area/$name.view.php") ?>
+        </div>
+        <hr class="divider">
+        <div class="aside-section">
+            <div class="logo">
+                <a href="/">
+                    <img src="public/assets/images/TechBubl.svg" alt="TechBubl Logo">
+                    <span>TechBubl</span>
+                </a>
+            </div>
+            <div class="popular-articles">
+                <h3>Popular Articles</h3>
+                <?php for ($i = 0; $i < 4; $i++) { ?>
+                    <a href="#">
+                    <div class="aside-article">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+                    </a>
+                <?php } ?>
+            </div>
+            <div class="most-recent">
+                <h3>Most Recent</h3>
+                <?php for ($i = 0; $i < 4; $i++) { ?>
+                    <a href="#">
+                    <div class="aside-article">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+                    </a>
+                <?php } ?>
+            </div>
+            <div class="newsletter">
+                <h3>Sign up for Daily Bubls</h3>
+                <span class="newsletter-info">A newsletter that offers bite-sized articles discussing various fields in the area of technology.</span>
+                <form class="signup-form" action="">
+                    <input type="email" name="signup-email" id="signup-email" placeholder="Email Address">
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <span class="footer-text">©2023, TechBubl</span>
+    </footer>
 </body>
 
 </html>
--- app/views/main/index.view.php
+++ app/views/main/index.view.php
@@ -1,1 +1,41 @@
-<!-- <h1>Home</h1> -->
+<div class="post-nav">
+    <div class="categories">
+        <a href="#">
+            <button class="category-btn">Cryptocurrency</button>
+        </a>
+        <a href="#">
+            <button class="category-btn">Web Development</button>
+        </a>
+        <a href="#">
+            <button class="category-btn">Artificial Intelligence</button>
+        </a>
+    </div>
+    <div class="sorting">
+        <select name="sortby" id="sortby" class="sortby">
+            <option value="most-recent">SORT BY: Most Recent</option>
+            <option value="most-popular">SORT BY: Most Popular</option>
+        </select>
+    </div>
+</div>
+
+<div class="post-grid">
+    <?php for ($i = 0; $i < 12; $i++) { ?>
+        <a href="#">
+            <div class="post-card">
+                <div class="post-author">
+                    <img class="post-author_img" src="public/assets/images/NoUser.svg" alt="Placeholder">
+                    <span class="post-author_name">Izzy Cantella</span>
+                </div>
+                <div class="post-content">
+                    <h4 class="post-title">My first ever post!</h4>
+                    <p class="post-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sed rem quod quaerat fuga deleniti consequuntur tempore obcaecati expedita reiciendis officia id ullam minus nulla, unde qui quasi quo? Corporis vero dolorum explicabo? Repudiandae, eum! Sapiente unde esse atque, soluta error libero modi quia cum rerum beatae suscipit autem magni repudiandae sunt, laudantium itaque architecto alias eos explicabo ad.</p>
+                </div>
+                <div class="post-info">
+                    <span class="post-category">Artificial Intelligence</span>
+                    <span class="seperator">·</span>
+                    <span class="post-read_time">6 mins</span>
+                </div>
+            </div>
+        </a>
+    <?php } ?>
+</div>
