bf5706c2d9131b8e524de50f6b757558af5713bf
kreid333 <kairetech@gmail.com>
2023-01-24 21:28:59 -0500

    changed css for contact view to bem methodology

--- app/views/main/contact.view.php
+++ app/views/main/contact.view.php
@@ -3,27 +3,26 @@
         <h1>CONTACT US</h1>
         <p>If you have any questions, concerns, or messages for us, please feel free to contact us.</p>
     </div>
-    <div class="contact-form">
-        <form action="">
-            <div class="name-field">
-                <div class="input-field">
-                    <label for="first-name">First Name</label>
-                    <input name="first-name" class="first-name" type="text" placeholder="First Name">
-                </div>
-                <div class="input-field">
-                    <label for="last-name">Last Name</label>
-                    <input name="last-name" class="last-name" type="text" placeholder="Last Name">
-                </div>
+
+    <form class="contact-form" action="">
+        <div class="contact-form__name-field">
+            <div class="contact-form__input-field">
+                <label for="first-name">First Name</label>
+                <input name="first-name" type="text" placeholder="First Name">
             </div>
-                <div class="input-field">
-                    <label for="email-address">Email Address</label>
-                    <input name="email-address" class="email-address" type="text" placeholder="Email Address">
-                </div>
-                <div class="input-field">
-                    <label for="message">Message</label>
-                    <textarea name="message" class="message" cols="30" rows="10" placeholder="Message"></textarea>
-                </div>
-            <button type="submit" class="submit-btn btn black">SUBMIT</button>
-        </form>
-    </div>
+            <div class="contact-form__input-field">
+                <label for="last-name">Last Name</label>
+                <input name="last-name" type="text" placeholder="Last Name">
+            </div>
+        </div>
+        <div class="contact-form__input-field">
+            <label for="email-address">Email Address</label>
+            <input name="email-address" type="text" placeholder="Email Address">
+        </div>
+        <div class="contact-form__input-field">
+            <label for="message">Message</label>
+            <textarea name="message" cols="30" rows="10" placeholder="Message"></textarea>
+        </div>
+        <button type="submit" class="btn btn--black contact-form__submit-btn">SUBMIT</button>
+    </form>
 </div>
