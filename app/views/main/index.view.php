7e1dbb6bf0a982b4a3573076d37dcda466277a92
kreid333 <kairetech@gmail.com>
2023-01-24 20:51:08 -0500

    changed css for index view to bem methodology

--- app/views/main/index.view.php
+++ app/views/main/index.view.php
@@ -1,39 +1,39 @@
-<div class="post-nav">
-    <div class="categories">
-        <a href="#">
-            <button class="category-btn">Cryptocurrency</button>
+<div class="posts-nav">
+    <div class="posts-nav__categories">
+        <a class="posts-nav__btn" href="#">
+            Cryptocurrency
         </a>
-        <a href="#">
-            <button class="category-btn">Web Development</button>
+        <a class="posts-nav__btn" href="#">
+            Web Development
         </a>
-        <a href="#">
-            <button class="category-btn">Artificial Intelligence</button>
+        <a class="posts-nav__btn" href="#">
+            Artificial Intelligence
         </a>
     </div>
-    <div class="sorting">
-        <select name="sortby" id="sortby" class="sortby">
+    <div class="posts-nav__sorting">
+        <select name="sortby">
             <option value="most-recent">SORT BY: Most Recent</option>
             <option value="most-popular">SORT BY: Most Popular</option>
         </select>
     </div>
 </div>
 
-<div class="post-grid">
+<div class="posts-grid">
     <?php for ($i = 0; $i < 12; $i++) { ?>
         <a href="#">
-            <div class="post-card">
-                <div class="post-author">
-                    <img class="post-author_img" src="public/assets/images/NoUser.svg" alt="Placeholder">
-                    <span class="post-author_name">Izzy Cantella</span>
+            <div class="post">
+                <div class="post__author">
+                    <img class="post__author-img" src="public/assets/images/NoUser.svg" alt="Placeholder">
+                    <span class="post__author-name">Izzy Cantella</span>
                 </div>
-                <div class="post-content">
-                    <h4 class="post-title">My first ever post!</h4>
-                    <p class="post-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sed rem quod quaerat fuga deleniti consequuntur tempore obcaecati expedita reiciendis officia id ullam minus nulla, unde qui quasi quo? Corporis vero dolorum explicabo? Repudiandae, eum! Sapiente unde esse atque, soluta error libero modi quia cum rerum beatae suscipit autem magni repudiandae sunt, laudantium itaque architecto alias eos explicabo ad.</p>
+                <div class="post__content">
+                    <h4 class="post__title">My first ever post!</h4>
+                    <p class="post__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sed rem quod quaerat fuga deleniti consequuntur tempore obcaecati expedita reiciendis officia id ullam minus nulla, unde qui quasi quo? Corporis vero dolorum explicabo? Repudiandae, eum! Sapiente unde esse atque, soluta error libero modi quia cum rerum beatae suscipit autem magni repudiandae sunt, laudantium itaque architecto alias eos explicabo ad.</p>
                 </div>
-                <div class="post-info">
-                    <span class="post-category">Artificial Intelligence</span>
-                    <span class="seperator">·</span>
-                    <span class="post-read_time">6 mins</span>
+                <div class="post__info">
+                    <span class="post__category">Artificial Intelligence</span>
+                    <span class="post__info--seperator">·</span>
+                    <span class="post__read-time">6 mins</span>
                 </div>
             </div>
         </a>
